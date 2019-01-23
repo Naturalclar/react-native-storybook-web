896f2e5cc8a0e0cab63bc01999921484ca69fb44
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-01-23 11:44:06 +0900

    :sparkles: add empty storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = (config, configType) => {
+  config.resolve = {
+    modules: ["node_modules"],
+    extensions: [".web.js", ".js", ".json", ".web.jsx", ".jsx"],
+    alias: {
+      "react-native": "react-native-web"
+    }
+  };
+
+  return config;
+};
