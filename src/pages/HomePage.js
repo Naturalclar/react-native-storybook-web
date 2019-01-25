a2b0f64477a6db771bb6028ca5f99042d7b01242
jesse <jesse.katsumata@cureapp.jp>
2019-01-25 12:50:19 +0900

    :sparkles: add template page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,12 @@
 // @flow
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Header } from "react-native-elements";
 import { LoginForm } from "../organisms";
-
-const styles = StyleSheet.create({
-  container: { flex: 1 }
-});
+import { Page } from "../templates";
 
 const HomePage = () => (
-  <View style={styles.container}>
-    <Header
-      leftComponent={{ icon: "menu", color: "#fff" }}
-      centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
-      rightComponent={{ icon: "home", color: "#fff" }}
-    />
+  <Page>
     <LoginForm />
-  </View>
+  </Page>
 );
 
 export default HomePage;
